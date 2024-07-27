1d3b8e0a520d2943e4ca2dde8b684edfb376b952
“gyeon” <jkp.112357@gmail.com>
2024-07-27 12:58:21 +0900

    update

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,11 +1,18 @@
 const tl = gsap.timeline({defaults: {ease: "power2.out"}});
 
 /* TEXT ANIMATION */
+// tl.to(".text", {
+//   y: "0%",
+//   duration: 1.5,
+//   stagger: 0.3,
+// });
+
 tl.to(".text", {
   y: "0%",
-  duration: 1.5,
-  stagger: 0.3,
+  duration: 2,
+  stagger: 0.2,
 });
+
 /* SLIDER OPTIONAL */
 // tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".onload", {
