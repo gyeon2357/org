b2af8558fe0c1b9a2e90cba12d6eda58a07701a7
“gyeon” <jkp.112357@gmail.com>
2024-08-24 13:20:45 +0900

    update

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,29 +1,30 @@
-const tl = gsap.timeline({defaults: {ease: "power2.out"}});
+const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
 
 /* TEXT ANIMATION */
-// tl.to(".text", {
-//   y: "0%",
-//   duration: 1.5,
-//   stagger: 0.3,
-// });
-
 tl.to(".text", {
   y: "0",
   opacity: 1,
-  duration: 1,
-  stagger: 0.2,
+  duration: 1.2,
+  stagger: 0.3,
 });
+
+// tl.to(".text", {
+//   y: "0",
+//   opacity: 1,
+//   duration: 1,
+//   stagger: 0.2,
+// });
 
 /* SLIDER OPTIONAL */
 // tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".onload", {
-   y: "0",
+  y: "0",
   opacity: "0",
   delay: "0.125",
-  duration: 0.6,
+  duration: 0.4,
 
   display: "none",
 }); /* ,  */
 
 /* HOME PAGE CONTENT ANIMATION */
-tl.fromTo("main, footer", {opacity: 0}, {opacity: 1, duration: 0.5});
+tl.fromTo("main, footer", { opacity: 0 }, { opacity: 1, duration: 0.5 });
