c6ed2bb9612f6900e909a00bdd32951e6b2c13af
“gyeon” <jkp.112357@gmail.com>
2024-08-24 14:18:39 +0900

    update

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -5,7 +5,7 @@ tl.to(".text", {
   y: "0",
   opacity: 1,
   duration: 1.2,
-  stagger: 0.3,
+  stagger: 0.25,
 });
 
 // tl.to(".text", {
@@ -21,10 +21,10 @@ tl.to(".onload", {
   y: "0",
   opacity: "0",
   delay: "0.125",
-  duration: 0.4,
+  duration: 0.3,
 
   display: "none",
 }); /* ,  */
 
 /* HOME PAGE CONTENT ANIMATION */
-tl.fromTo("main, footer", { opacity: 0 }, { opacity: 1, duration: 0.5 });
+tl.fromTo("main, footer", { opacity: 0 }, { opacity: 1, duration: 0.3 });
