91ec3e9b00597542e9cf5e67fb82a22d2c3aa36b
“gyeon” <jkp.112357@gmail.com>
2024-09-07 00:23:18 +0900

    update

--- practice/sketch/map/script.js
+++ practice/sketch/map/script.js
@@ -1,69 +1,66 @@
 function initialize() {
+  var myCenter = new google.maps.LatLng(38.9047, -77.0164);
 
-    var myCenter = new google.maps.LatLng(38.9047, -77.0164);
+  var mapProp = {
+    center: myCenter,
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
 
-    var mapProp = {
-        center: myCenter,
-        zoom: 7,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+  map = new google.maps.Map(document.getElementById("gMap"), mapProp);
 
-    map = new google.maps.Map(document.getElementById("gMap"), mapProp);
+  google.maps.event.addListener(map, "click", function (event) {
+    placeMarker(event.latLng);
+    saveMarker(event);
+  });
 
-    google.maps.event.addListener(map, 'click', function (event) {
-        placeMarker(event.latLng);
-        saveMarker(event);
-    });
+  //map.data.setControls(['Point']);
+  bindDataLayerListeners(map.data);
 
-
-
-    //map.data.setControls(['Point']);
-    bindDataLayerListeners(map.data);
-
-    //load saved data
-    loadMarkers(map);
-
+  //load saved data
+  loadMarkers(map);
 }
 
 function placeMarker(location) {
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map,
-    });
-    var infowindow = new google.maps.InfoWindow({
-        content: 'Latitude: ' + location.lat() + '<br>Longitude: ' + location.lng()
-    });
-    infowindow.open(map, marker);
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+  });
+  var infowindow = new google.maps.InfoWindow({
+    content: "Latitude: " + location.lat() + "<br>Longitude: " + location.lng(),
+  });
+  infowindow.open(map, marker);
 
-    map.data.add(new google.maps.Data.Feature({properties:{},geometry:new google.maps.Data.Point(location)}));
+  map.data.add(
+    new google.maps.Data.Feature({
+      properties: {},
+      geometry: new google.maps.Data.Point(location),
+    })
+  );
 }
-
 
 // Apply listeners to refresh the GeoJson display on a given data layer.
 function bindDataLayerListeners(dataLayer) {
-    dataLayer.addListener('addfeature', saveMarker);
-    dataLayer.addListener('removefeature', saveMarker);
-    //dataLayer.addListener('setgeometry', saveMarker);
+  dataLayer.addListener("addfeature", saveMarker);
+  dataLayer.addListener("removefeature", saveMarker);
+  //dataLayer.addListener('setgeometry', saveMarker);
 }
-
-
 
 function saveMarker() {
-    map.data.toGeoJson(function (json) {
-        localStorage.setItem('geoData', JSON.stringify(json));
-    });
+  map.data.toGeoJson(function (json) {
+    localStorage.setItem("geoData", JSON.stringify(json));
+  });
 }
 
-
 function clearMarkers() {
-    map.data.forEach(function (f) {
-        map.data.remove(f);
-    });
+  map.data.forEach(function (f) {
+    map.data.remove(f);
+  });
 }
 
 function loadMarkers(map) {
-    var data = JSON.parse(localStorage.getItem('geoData'));
-    map.data.addGeoJson(data);
+  var data = JSON.parse(localStorage.getItem("geoData"));
+  map.data.addGeoJson(data);
 }
 
 //google.maps.event.addDomListener(window, 'load', initialize);
