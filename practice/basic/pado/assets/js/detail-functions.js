5da412f4003c696111956e9bc3bbfc12c58c59b9
“gyeon” <jkp.112357@gmail.com>
2024-11-12 11:18:09 +0900

    update

--- practice/basic/pado/assets/js/detail-functions.js
+++
@@ -1,7 +0,0 @@
-// project total num
-
-$(function () {
-  var count = $(".slideshow__gallery .slideshow__item").length;
-  var sliderCounterTotal = $(".counter--total");
-  $(sliderCounterTotal).html("/ " + count);
-});
