d96ffeee1ed1600bffe86700436a1df2746e6197
“gyeon” <jkp.112357@gmail.com>
2024-12-16 20:51:20 +0900

    update

---
+++ practice/basic/passport/assets/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $("input[name = 'printName']").focus();
+  $("#backg").attr("loop", "true");
+});
+
+// var backg = document.getElementById("backg");
+// if (typeof backg.loop == "boolean") {
+//   // loop supported
+//   backg.loop = true;
+// } else {
+//   // loop property not supported
+//   backg.on(
+//     "ended",
+//     function () {
+//       this.currentTime = 0;
+//       this.play();
+//     },
+//     false
+//   );
+// }
+// backg.play();
+
+// input txt
+function printName() {
+  const name = document.getElementById("name").value;
+  document.getElementById("result").innerText = name;
+}
+
+// enter
+var input = document.getElementById("name");
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("print-btn").click();
+  }
+});
