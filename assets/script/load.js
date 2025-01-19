96e83df6edc3915d257f9985ce6fa440fa99a9b1
“gyeon” <jkp.112357@gmail.com>
2025-01-19 20:32:58 +0900

    update

---
+++ assets/script/load.js
@@ -0,0 +1,18 @@
+const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
+
+/* TEXT ANIMATION */
+tl.to(".text", {
+  y: "0%",
+  duration: 1.5,
+  stagger: 0.4,
+});
+/* SLIDER OPTIONAL */
+// tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", {
+  y: "-100%",
+  delay: 0.25,
+  duration: 0.9,
+}); /* ,  */
+
+/* HOME PAGE CONTENT ANIMATION */
+// tl.fromTo("main", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
