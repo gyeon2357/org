d05f1c6ff10f601d31ae5777516aa2a0e658fda4
“gyeon” <jkp.112357@gmail.com>
2025-02-12 17:12:12 +0900

    update

--- assets/script/load.js
+++ assets/script/load.js
@@ -1,30 +1,46 @@
 const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
 
 /* TEXT ANIMATION */
-// tl.to(".intro-box", {
-//   background: "",
-//   height: "120px",
-//   duration: 0.8,
-//   delay: 0.1,
-// });
+
 tl.to(".line", {
-  width: "120px",
-    opacity: "1",
-  duration: 0.4,
-  stagger: 0.125,
-});
-tl.to(".line-text", {
+  // width: "120px",
+  y: "0",
   opacity: "1",
-  duration: 0.2,
-  stagger: 0.125,
+  delay: 0.25,
+  duration: 0.5,
+  stagger: 0.15,
 });
 
-tl.to(".text", {
-  y: "0%",
-  duration: 0.875,
-  stagger: 0.15,
-  delay: 0.4,
-});
+// tl.to(".line-text", {
+//   opacity: "1",
+//   duration: 0.2,
+//   stagger: 0.125,
+// });
+
+tl.fromTo(
+  ".text",
+  {
+    x: 10,
+    opacity: 0,
+  },
+  {
+    // delay: 2,
+    duration: 1,
+    x: 0,
+    opacity: 1,
+    stagger: {
+      from: "start",
+      amount: 0.5,
+    },
+  }
+);
+
+// tl.to(".text", {
+//   y: "0%",
+//   duration: 0.75,
+//   stagger: 0.15,
+//   delay: 0.15,
+// });
 /* SLIDER OPTIONAL */
 
 tl.to(".intro", {
---
+++ practice/sketch/text-trail-effect-master/README.md
@@ -0,0 +1,46 @@
+# Creating Typography Motion Trail Effects with WebGL
+
+Using WebGL framebuffers via Three.js for advanced graphical effects.
+
+![Image Title](https://tympanus.net/codrops/wp-content/uploads/2021/07/motiontrail_feat.jpg)
+
+[Article on Codrops](https://tympanus.net/codrops/?p=55545)
+
+[Demo](http://tympanus.net/Tutorials/text-trail-effect/)
+
+
+## Installation
+
+Install dependencies:
+
+```
+npm install
+```
+
+Compile the code for development and start a local server:
+
+```
+npm start:local
+```
+
+Create the build:
+
+```
+npm run build
+```
+
+## Misc
+
+Follow Georgi: [Twitter](https://twitter.com/georgiNikoloff), [GitHub](https://github.com/gnikoloff) 
+
+Follow Codrops: [Twitter](http://www.twitter.com/codrops), [Facebook](http://www.facebook.com/codrops), [GitHub](https://github.com/codrops), [Instagram](https://www.instagram.com/codropsss/)
+
+## License
+[MIT](LICENSE)
+
+Made with :blue_heart:  by [Codrops](http://www.codrops.com)
+
+
+
+
+
