2c2b04519eb6d943b5dfdecc0a969cbe3b70f3cb
“gyeon” <jkp.112357@gmail.com>
2025-02-27 12:40:04 +0900

    update

--- assets/script/load.js
+++ assets/script/load.js
@@ -2,55 +2,55 @@ const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
 
 /* TEXT ANIMATION */
 
-tl.to(".line", {
-  y: "0",
-  opacity: "1",
-  delay: 0.15,
-  duration: 0.4,
-  stagger: 0.1,
-});
+// tl.to(".i-project", {
+//   y: "0",
+//   opacity: "1",
+//   delay: 0.15,
+//   duration: 0.4,
+//   stagger: 0.1,
+// });
 
-tl.fromTo(
-  ".text",
-  {
-    x: 5,
-    opacity: 0,
-  },
-  {
-    duration: 0.5,
-    x: 0,
-    opacity: 1,
-    stagger: {
-      from: "start",
-      amount: 0.5,
-    },
-  }
-);
+// tl.fromTo(
+//   ".split",
+//   {
+//     x: 5,
+//     opacity: 0,
+//   },
+//   {
+//     duration: 0.5,
+//     x: 0,
+//     opacity: 1,
+//     stagger: {
+//       from: "start",
+//       amount: 0.5,
+//     },
+//   }
+// );
 
-tl.to(".intro", {
-  opacity: "0",
-  delay: 0.125,
-  duration: 0.5,
-  display: "none",
-});
+// tl.to(".loading", {
+//   opacity: "0",
+//   duration: 0.5,
+//   display: "none",
+// });
 
 /* GRID ANIMATION */
 
-// .to(".gallery-container .project::after", {
-//   opacity: "1",
-//   delay: 0.25,
-//   duration: 0.25,
-// });
-
-
-
 tl.add("start")
 
+  .to(
+    ".nav",
+    {
+      opacity: 1,
+      duration: 0.5,
+    },
+    "start"
+  )
   .to(
     ".gallery-container .project > a",
     {
       opacity: "1",
       duration: 0.5,
+      delay: 0.5,
       stagger: 0.12,
     },
     "start"
@@ -61,6 +61,7 @@ tl.add("start")
     {
       opacity: "1",
       duration: 0.5,
+            delay: 0.5,
       stagger: 0.03,
     },
     "start"
